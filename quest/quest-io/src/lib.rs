dde2bee7528dcd1944987379f49fa35fdd852d2b
lhymmEU <italy.18429@gmail.com>
2023-10-22 21:33:18 -0400

    Actions implemented: publish, claim, submit

--- quest/quest-io/src/lib.rs
+++ quest/quest-io/src/lib.rs
@@ -16,39 +16,60 @@ impl Metadata for ProgramMetadata {
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub struct Quest {
 	// id of a quest, needs to change to a dedicated type in the future
-	id: String,
+	pub id: u32,
 	// possible status of a quest
-	status: QuestStatus,
+	pub status: QuestStatus,
 	// recruiters who published the quest
-	publisher: ActorId,
+	pub publisher: ActorId,
 	// title of the quest, the type should be changed to a dedicated type for better security control
-	title: String,
+	pub title: String,
 	// possible positions that a professor can offer
-	position: Position,
+	pub position: Position,
 	// deadline in the format of exec::block_height(),
 	// after the deadline has passed, status will automatically change to closed,
 	// no more claims and submissions allowed after the deadline. 
-	deadline: u32,
+	pub deadline: u32,
 	// a string of the uri of the image,
 	// None means no image for this quest
-	img: Option<String>,
+	pub img: Option<String>,
 	// descriptions of deliverables of this quest
 	// this field is only displayed to seekers
-	deliverables: Vec<String>,
+	pub deliverables: Vec<String>,
 	// String represents the uri of the actual submission,
 	// for now we only accept one uri per seeker,
 	// should use IPFS cid and accept more uris per seeker in the future.
-	seeker_submission: BTreeMap<ActorId, String>,
+	pub seeker_submission: BTreeMap<ActorId, String>,
 	// seeker's status of a quest
-	seeker_status: BTreeMap<ActorId, Status>,
+	pub seeker_status: BTreeMap<ActorId, Status>,
 	// urls to the published ads,
 	// this field is only displayed to recruiters
-	ads_links: Ads,
+	pub ads_links: Ads,
 	// this probably needs to change to a dedicated type for better screen display
-	details: String,
+	pub details: String,
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
+impl Quest {
+	// method to check if a quest is complete
+	pub fn is_complete(&self) -> bool {
+		!self.title.is_empty()
+			&& !self.deliverables.is_empty()
+			&& !self.details.is_empty()
+	}
+	// check if a quest is open
+	pub fn is_open(&self) -> bool {
+		self.status == QuestStatus::Open
+	}
+	// check if a quest is closed
+	pub fn is_closed(&self) -> bool {
+		self.status == QuestStatus::Closed
+	}
+	// check if a seeker's current status matches the status given
+	pub fn seeker_status_match(&self, key: &ActorId, status: Status) -> bool {
+		*self.seeker_status.get(key).unwrap() == status
+	}
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo, PartialEq, Eq)]
 pub enum QuestStatus {
 	Open,
 	Full,
@@ -63,8 +84,9 @@ pub enum Position {
 	PostDoc,
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
+#[derive(Debug, Encode, Decode, TypeInfo, PartialEq, Eq)]
 pub enum Status {
+	Claimed,
 	WaitingReply,
 	InterviewReceived,
 	InterviewAccepted,
@@ -83,20 +105,40 @@ pub struct Ads {
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum QuestAction {
+	/// Recruiters publish a quest.
+	/// 
+	/// Requirements:
+	/// * The msg sender must be an approved recruiter.
+	/// * All fields of a quest must be filled.
+	/// 
+	/// Arguments:
+	/// * quest: the quest to be published.
 	Publish {
-		publisher: ActorId,
-		quest: Vec<u8>,
+		quest: Quest,
 	},
+	/// Seekers claim a quest.
+	/// 
+	/// Requirements:
+	/// * the quest of the given id must exist.
+	/// * the quest must be open.
+	/// 
+	/// Arguments:
+	/// * quest_id: the id of the quest to be claimed.
 	Claim {
-		// the seeker's id who claimed the quest
-		seeker: ActorId,
-		// which quest is claimed
-		quest_id: String,
+		quest_id: u32,
 	},
+	/// Seekers submit their submissions.
+	/// 
+	/// Requirements:
+	/// * the quest of the given id must exist.
+	/// * the quest must NOT be closed.
+	/// * the seeker must have claimed the quest.
+	/// 
+	/// Arguments:
+	/// * quest_id: the id of the quest to be claimed.
+	/// * submission: the submission of the seeker.
 	Submit {
-		// the seeker's id who submitted to the quest
-		seeker: ActorId,
-		quest_id: String,
+		quest_id: u32,
 		// the external url of the submission, e.g. Google drive link
 		// this should be changed to IPFS cid in the future
 		submission: String,
@@ -104,20 +146,31 @@ pub enum QuestAction {
 	Interview {
 		// the id of the seeker who will be interviewed
 		seeker: ActorId,
-		quest_id: String,
+		quest_id: u32,
 	},
 	Offer {
 		// the id of the seeker who is getting an offer
 		seeker: ActorId,
-		quest_id: String,
+		quest_id: u32,
 	},
 	Reject {
 		seeker: ActorId,
-		quest_id: String,
+		quest_id: u32,
 	},
 	Close {
-		quest_id: String,
-	}
+		quest_id: u32,
+	},
+	/// Add reruiters to the approved recruiters list.
+	/// 
+	/// Requirements:
+	/// * The sender must be the owner of the quest contract.
+	/// * The recruiter must not be in the approved recruiters list.
+	/// 
+	/// Arguments:
+	/// * recruiter: the id of the recruiter to be added.
+	AddRecruiter {
+		recruiter: ActorId,
+	},
 }
 
 #[derive(Encode, Decode, TypeInfo)]
--- quest/src/lib.rs
+++ quest/src/lib.rs
@@ -1,18 +1,23 @@
 #![no_std]
-use gstd::{msg, prelude::*, debug, collections::BTreeMap, ActorId};
+use gstd::{msg::{self, source}, prelude::*, debug, collections::BTreeMap, ActorId, exec};
 use quest_io::*;
 
 struct Quests {
 	// the publisher of the quest contract
 	owner: ActorId,
 	// the address of the account's master contract
-	account_contract_addr: ActorId,
+	account_contract_addr: Option<ActorId>,
+    // the list of approved recruiters
+    approved_recruiters: Vec<ActorId>,
 	// maximum number of quests a single quest storage contract should handle
 	max_num_quests: u32,
 	// current number of quests in this contract
 	num_counter: u32,
-	// String represents quest id
-	quests: BTreeMap<String, Quest>,
+    // TODO: ideally, this should be a hash over the quest content
+    // now it's just a counter that will only increase when a new quest is published
+    quest_id: u32,
+	// u32 represents quest id
+	quests: BTreeMap<u32, Quest>,
 }
 
 static mut CONTRACT: Option<Quests> = None;
@@ -20,11 +25,138 @@ static mut CONTRACT: Option<Quests> = None;
 #[no_mangle]
 extern "C" fn init() {
     let _init_info: String = msg::load().expect("Failed to load init info");
+    unsafe {
+        CONTRACT = Some(Quests {
+            owner: msg::source(),
+            account_contract_addr: None,
+            approved_recruiters: Vec::new(),
+            max_num_quests: 1000,
+            num_counter: 0,
+            quest_id: 0,
+            quests: BTreeMap::new(),
+        });
+    }
 
     let _ = msg::reply(QuestEvent::ContractInitiated, 0);
 }
 
 #[no_mangle]
 extern "C" fn handle() {
-    
+    let action: QuestAction = msg::load().expect("Failed to load action");
+    let quests: &mut Quests = unsafe { CONTRACT.as_mut().expect("Quest contract not initialized.") };
+
+    match action {
+        QuestAction::Publish { quest } => {
+            // 0. check if the max number of quests is reached
+            if quests.num_counter >= quests.max_num_quests {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Publish"), 
+                    reason: String::from("Max number of quests reached."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 1. check if the publisher is approved
+            if !quests.approved_recruiters.contains(&msg::source()) {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Publish"), 
+                    reason: String::from("Publisher is not approved."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 2. check if the quest is valid
+            if !quest.is_complete() {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Publish"), 
+                    reason: String::from("Quest is not complete."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 3. publish the quest
+            let quest_id = quests.quest_id;
+            quests.quest_id += 1;
+            quests.num_counter += 1;
+            quests.quests.insert(quest_id, quest);
+
+            let _ = msg::reply(QuestEvent::OperationSuccess { 
+                name: String::from("Publish"), 
+                timestamp: exec::block_timestamp(),
+            }, 0);
+        },
+        QuestAction::Claim { quest_id } => {
+            // 1. check if the quest exists
+            if !quests.quests.contains_key(&quest_id) {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Claim"), 
+                    reason: String::from("Quest does not exist."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 2. check if the quest is open
+            if !quests.quests.get(&quest_id).unwrap().is_open() {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Claim"), 
+                    reason: String::from("Quest is not open."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 3. claim the quest
+            let quest = quests.quests.get_mut(&quest_id).unwrap();
+            // create an empty submission for the seeker
+            quest.seeker_submission.insert(msg::source(), String::new());
+            // enter the seeker into the seeker status map
+            quest.seeker_status.insert(msg::source(), Status::Claimed);
+
+            let _ = msg::reply(QuestEvent::OperationSuccess { 
+                name: String::from("Claim"), 
+                timestamp: exec::block_timestamp(), 
+            }, 0);
+        },
+        QuestAction::Submit { quest_id, submission } => {
+            // 1. check if the quest exists
+            if !quests.quests.contains_key(&quest_id) {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Submit"), 
+                    reason: String::from("Quest does not exist."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+
+            let quest = quests.quests.get_mut(&quest_id).unwrap();
+
+            // 2. check if the quest is NOT closed
+            if quest.is_closed() {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Submit"), 
+                    reason: String::from("Quest is closed."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 3. seekers can only submit if their current status is claimed,
+            // this means the seeker has already claimed the quest but has not submitted yet
+            if !quest.seeker_status_match(&msg::source(), Status::Claimed) {
+                let _ = msg::reply(QuestEvent::OperationErr { 
+                    name: String::from("Submit"), 
+                    reason: String::from("Seeker is either not claimed this quest or has already submitted."),
+                    timestamp: exec::block_timestamp(),
+                }, 0);
+                return;
+            }
+            // 4. submit the quest
+            quest.seeker_submission.insert(msg::source().clone(), submission);
+            quest.seeker_status.insert(msg::source(), Status::WaitingReply);
+            let _ = msg::reply(QuestEvent::OperationSuccess { 
+                name: String::from("Submit"), 
+                timestamp: exec::block_timestamp(), 
+            }, 0);
+        },
+        _ => {},
+    }
+
 }
