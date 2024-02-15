fd4c0543cd5388a0ad43de40aee385e9f72f4463
lhymmEU <italy.18429@gmail.com>
2024-02-15 23:52:04 +0800

    reputation contract PoC complete

--- reputation/reputation-io/src/lib.rs
+++ reputation/reputation-io/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
-use gstd::{ prelude::*, ActorId, collections::HashMap };
+
+use gstd::{ collections::HashMap, prelude::*, ActorId };
 use gmeta::{In, InOut, Out, Metadata};
 
 pub struct ProgramMetadata;
@@ -20,22 +21,87 @@ pub struct MtkData {
     pub name: String,
     pub symbol: String,
     pub base_uri: String,
+    pub available_skill_names: AvailableSkillNames,
+    // data fields for skill fungible tokens
     pub balances: HashMap<TokenId, HashMap<ActorId, u128>>,
-    pub matching_pairs: HashMap<TokenId, TokenId>,
-    pub fungible_tokens: Vec<TokenId>,
-    pub skill_nfts: Vec<TokenId>,
-    pub repu_nfts: Vec<TokenId>,
-    // for nft
-    pub token_metadata: HashMap<TokenId, TokenMetadata>,
-    pub owners: HashMap<TokenId, ActorId>,
+    pub skill_fungible_tokens: HashMap<TokenId, SkillFtData>,
+    pub ft_owners: HashMap<ActorId, Vec<TokenId>>,
+    // for skill nfts
+    pub skill_nft_metadata: HashMap<TokenId, SkillNftMetadata>,
+    pub nft_owners: HashMap<ActorId, Vec<TokenId>>,
 }
 
+// For skill fungible tokens.
 #[derive(Debug, Decode, Encode, TypeInfo, Default, Clone, PartialEq, Eq)]
-pub struct TokenMetadata {
+pub struct SkillFtData {
+    // e.g. Biomedicine
+    pub name: Option<String>,
+	// Location of the symbol
+	pub symbol: Option<String>,
+	// Total circulation of the token
+	pub circulation: Option<u128>,
+}
+
+#[derive(Debug, Decode, Encode, TypeInfo, Default)]
+pub struct SkillNftMetadata {
+	// The receipient's id
+	pub owner: Option<ActorId>,
+	// The name of the skill NFT
+	pub title: Option<String>,
+	// This should be an overview of the quest that issued this NFT
+	pub description: Option<String>,
+	// All the information of the quest, including the submission from the NFT receipient.
+	pub quest_details: Option<Quest>,
+	// Freshness score denominated in block height
+	pub freshness: Option<u32>,
+}
+
+#[derive(Debug, Decode, Encode, TypeInfo, Default)]
+// All possible names of skill NFTs we can issue.
+pub struct AvailableSkillNames {
+    names: Vec<String>,
+}
+
+impl AvailableSkillNames {
+    pub fn new() -> Self {
+        AvailableSkillNames {
+            names: Vec::new(),
+        }
+    }
+
+    pub fn add_name(&mut self, name: String) {
+        self.names.push(name);
+    }
+
+    pub fn remove_name(&mut self, name: &String) -> bool {
+        if !self.check_name(name) {
+            // return false if the name does not exists
+            return false;
+        }
+
+        self.names.retain(|n| n != name);
+        true
+    }
+
+    pub fn check_name(&self, name: &String) -> bool {
+        self.names.contains(name)
+    }
+}
+
+#[derive(Encode, Decode, Debug, TypeInfo, Default)]
+pub struct Quest {
+    // The quest's id
+    pub id: Option<String>,
+    // The quest's publisher
+    pub publisher: Option<ActorId>,
+    // The quest's title
     pub title: Option<String>,
+    // The quest's description
     pub description: Option<String>,
-    pub media: Option<String>,
-    pub reference: Option<String>,
+    // The quest's reward
+    pub reward: Option<TokenId>,
+    // The submission storage id `String` that resulted in the issuance of the skill NFT.
+    pub submission: Option<String>,
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
@@ -50,67 +116,166 @@ pub struct InitMTK {
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub enum MTKAction {
-    /// Mints a skill ft or reputation nft to a user.
+    /// Mint a skill ft to a user.
     ///
     /// # Requirements:
-    /// * if minting a reputation NFT `amount` MUST equal to 1.
     /// * only existing tokens can be minted.
     ///
-    /// On success returns `MTKEvent::MintTo`.
-    Mint {
+    /// On success returns `MTKEvent::SkillTokenMinted`.
+    MintFtTo {
         /// Token id.
         id: TokenId,
         /// Token amount.
         amount: u128,
         /// Receipient.
         to: ActorId,
-        /// Token metadata, applicable if minting an NFT.
-        token_metadata: Option<TokenMetadata>,
     },
-    // Transform skill ft to matching nft.
+    // Burn skill fts to gain entrance to mid-tier quests.
     //
     // # Requirements:
-    // * at the fresh exchange rate, seeker must holds enough fts for `amount` of nfts.
-    // * the `skill_ft` and `skill_nft` must in the matching pair.
+    // * the account `from` must holds enough `amount` fts
     //
-    // On success returns `MTKEvent::SkillNftAcquired`.
-    /*
-    BurnToNFT {
-        /// Seeker.
+    // On success returns `MTKEvent::SkillTokenBurned`.
+    Burn {
+        // The skill ft's id.
+        id: TokenId,
+        // The account to burn.
         from: ActorId,
-        /// The skill ft intended to spend.
-        skill_ft: TokenId,
-        /// The skill nft intended to acquire.
-        skill_nft: TokenId,
-        /// The amount of skill nfts intended to acquire.
+        // The amount of skill fts to burn.
         amount: u128,
-    }
-    */
-    // -----------------Below are admin functions-----------------
-    // Add a new token to the multitoken.
-    AddSkillFt {
-        // Token id.
-        ft_id: TokenId,
-        // The matching skill nft's id.
-        nft_id: TokenId,
+    },
+    /// Mint a new skill nft to receipient.
+    ///
+    /// On success returns `MTKEvent::SkillNftMinted`.
+    MintNftTo {
+        // The receipient.
+        to: ActorId,
+        // Metadata for the skill nft.
+        metadata: SkillNftMetadata,
+    },
+
+    /// Verify a user's reputation using its on-chain address.
+    ///
+    /// # Requirements:
+    /// * the id to be verified must exists.
+    ///
+    /// On success returns `MTKEvent::RepuVerified`.
+    VerifyReputation {
+        // Which user to verify
+        target: ActorId,
+        // What reputation type to verify: skill ft or nft
+        // `true` - nft, `false` - ft
+        skill_type: bool,
+        // what reputation to verify
+        token_id: TokenId,
+    },
+    
+
+    // -----------------Below are functions that must go through OpenGov in the future-----------------
+    
+    /// Add a new skill FT.
+    /// 
+    /// # Requirements:
+    /// * only contract creator can add new skill fts.
+    /// 
+    /// On success returns `MTKEvent::NewFtAdded`.
+    AddFt {
+        // The metadata of the newly added skill token.
+        token_data: SkillFtData,
+    },
+
+    /// Change the information of a skill FT.
+    ///
+    /// # Requirements:
+    /// * token id must exists.
+    /// * only contract creater can change the information.
+    ///
+    /// On success returns `MTKEvent::SkillFtChanged`.
+    ChangeFt {
+        // Which token to change.
+        id: TokenId,
+        // The metadata after changing.
+        new_data: SkillFtData,
+    },
+
+    /// Add a new possible name for skill NFTs.
+    ///
+    /// # Requirements:
+    /// * only contract creator can add new skill NFT.
+    ///
+    /// On success returns `MTKEvent::SkillNftAdded`.
+    AddNft {
+        // The new name.
+        name: String,
+    },
+
+    /// Render an NFT not issuable anymore.
+    /// Notice: it simply removes the name from the available names set so it can't get issued in the future.
+    ///
+    /// # Requirements:
+    /// * the name to be removed must exists.
+    /// * only contract creator can remove a skill NFT.
+    ///
+    /// On success returns `MTKEvent::SkillNftRemoved`.
+    RemoveNft {
+        // Which NFT name to remove.
+        name: String,
+    },
+
+    
+    /// Change the base URI of the multitoken.
+    /// 
+    /// # Requirements:
+    /// * only contract creator can change the base URI.
+    ///
+    /// On success returns `MTKEvent::MtkUriChanged`.
+    ChangeBaseUri {
+        new_base_uri: String,
     },
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub enum MTKEvent {
-    MintTo {
-        to: ActorId,
+    SkillTokenMinted {
         id: TokenId,
-        amounts: u128,
+        amount: u128,
+        to: ActorId,
     },
-    SkillNftAcquired {
-        receipient: ActorId,
-        skill_nft: TokenId,
+    SkillTokenBurned {
+        from: ActorId,
+        id: TokenId,
         amount: u128,
     },
+    SkillNftMinted {
+        id: TokenId,
+        to: ActorId,
+    },
+    RepuVerified {
+        // The address which initiated the verification.
+        initiator: ActorId,
+        // The target address that get verified.
+        target: ActorId,
+    },
+    RepuVerificationFail {
+        // The address which initiated the verification.
+        initiator: ActorId,
+        // The target address that get verified.
+        target: ActorId,
+    },
     NewFtAdded {
-        ft_id: TokenId,
-        nft_id: TokenId,
+        id: TokenId,
+    },
+    SkillFtChanged {
+        id: TokenId,
+    },
+    SkillNftAdded {
+        name: String,
+    },
+    SkillNftRemoved {
+        name: String,
+    },
+    MtkUriChanged {
+        new_uri: String,
     },
 }
 
@@ -118,6 +283,10 @@ pub enum MTKEvent {
 pub enum MTKError {
     TokenDoesNotExists,
     TokenAlreadyExists,
+    OnlyCreaterCanOperate,
+    InsufficientBalance,
+    OwnerDoesNotExists,
+    SkillNameDoesNotExists,
     NotImplemented,
 }
 
@@ -126,42 +295,11 @@ pub struct State {
     pub name: String,
     pub symbol: String,
     pub base_uri: String,
-    pub balances: Vec<(TokenId, Vec<(ActorId, u128)>)>,
-    // matching pairs for skill ft and nft
-    pub matching_pairs: Vec<(TokenId, TokenId)>,
-    pub ft_tokens: Vec<TokenId>,
-    pub skill_nft_tokens: Vec<TokenId>,
-    pub repu_nft_tokens: Vec<TokenId>,
-    pub token_metadata: Vec<(TokenId, TokenMetadata)>,
-    // owner for nft
-    pub owners: Vec<(TokenId, ActorId)>,
     pub creator: ActorId,
-    pub supply: Vec<(TokenId, u128)>,
-}
-
-impl State {
-    pub fn tokens_ids_for_owner(&self, owner: &ActorId) -> Vec<TokenId> {
-        let mut tokens: Vec<TokenId> = Vec::new();
-        let balances = &self.balances;
-        for (token, bals) in balances {
-            if bals.iter().any(|(id, _b)| owner.eq(id)) {
-                tokens.push(*token);
-            }
-        }
-        tokens
-    }
-    pub fn get_balance(&self, account: &ActorId, id: &TokenId) -> u128 {
-        if let Some((_token_id, balances)) = self
-            .balances
-            .iter()
-            .find(|(token_id, _balances)| id.eq(token_id))
-        {
-            if let Some((_owner, balance)) =
-                balances.iter().find(|(owner, _balance)| account.eq(owner))
-            {
-                return *balance;
-            }
-        }
-        0
-    }
+    pub available_skill_names: AvailableSkillNames,
+    pub balances: Vec<(TokenId, Vec<(ActorId, u128)>)>,
+    pub skill_fungible_tokens: Vec<(TokenId, SkillFtData)>,
+    pub ft_owners: Vec<(ActorId, Vec<TokenId>)>,
+    pub skill_nft_metadata: Vec<(TokenId, SkillNftMetadata)>,
+    pub nft_owners: Vec<(ActorId, Vec<TokenId>)>,
 }
--- reputation/src/lib.rs
+++ reputation/src/lib.rs
@@ -1,12 +1,11 @@
 #![no_std]
-use gstd::{collections::HashMap, msg, prelude::*, ActorId};
+use gstd::{collections::HashMap, exec, msg, prelude::*, ActorId};
 use reputation_io::*;
 
 #[derive(Debug, Default)]
 pub struct Mtk {
     pub tokens: MtkData,
     pub creator: ActorId,
-    pub supply: HashMap<TokenId, u128>,
 }
 
 static mut CONTRACT: Option<Mtk> = None;
@@ -39,16 +38,20 @@ extern "C" fn handle() {
     let mtk_contract = unsafe { CONTRACT.as_mut().expect("`Mtk` is not initialized.") };
 
     let reply = match action {
-        MTKAction::Mint {
-            id,
-            amount,
-            to,
-            token_metadata,
-        } => mtk_contract.mint(id, amount, to, token_metadata),
-
-        MTKAction::AddSkillFt { ft_id, nft_id } => mtk_contract.add_ft(ft_id, nft_id),
+        MTKAction::MintFtTo { id, amount, to } => mtk_contract.mint_ft_to(id, amount, to),
+        MTKAction::MintNftTo { to, metadata } => mtk_contract.mint_nft_to(to, metadata),
+        MTKAction::Burn { id, from, amount } => mtk_contract.burn(id, from, amount),
+        MTKAction::ChangeBaseUri { new_base_uri } => mtk_contract.change_base_uri(new_base_uri),
+        MTKAction::AddFt { token_data } => mtk_contract.add_ft(token_data),
+        MTKAction::ChangeFt { id, new_data } => mtk_contract.change_ft(id, new_data),
+        MTKAction::AddNft { name } => mtk_contract.add_nft(name),
+        MTKAction::RemoveNft { name } => mtk_contract.remove_nft(name),
+        MTKAction::VerifyReputation {
+            target,
+            skill_type,
+            token_id,
+        } => mtk_contract.verify_repu(target, skill_type, token_id),
     };
-
     msg::reply(reply, 0).expect("Failed to encode or reply with `Result<MtkEvent, MtkError>`.");
 }
 
@@ -61,42 +64,195 @@ extern "C" fn state() {
 }
 
 impl Mtk {
-    fn mint(
-        &mut self,
-        id: TokenId,
-        amounts: u128,
-        to: ActorId,
-        _metadata: Option<TokenMetadata>,
-    ) -> Result<MTKEvent, MTKError> {
+    fn mint_ft_to(&mut self, id: TokenId, amount: u128, to: ActorId) -> Result<MTKEvent, MTKError> {
         // 1. check if the token id exists
-        if self.tokens.fungible_tokens.contains(&id) {
+        if self.tokens.skill_fungible_tokens.contains_key(&id) {
             // 2. if the receipient exists, add to the existing balance; else create a new balance
             let owner_balances = self.tokens.balances.get_mut(&id).unwrap();
             if let Some((_owner, balance)) =
                 owner_balances.iter_mut().find(|(owner, _)| to.eq(owner))
             {
-                *balance += amounts;
+                *balance += amount;
             } else {
-                owner_balances.insert(to, amounts);
+                owner_balances.insert(to.clone(), amount);
+                // add the new token to this owner's list of fts
+                self.tokens.ft_owners.entry(to.clone()).or_insert_with(Vec::new).push(id.clone());
             }
-            return Ok(MTKEvent::MintTo { to, id, amounts });
+            return Ok(MTKEvent::SkillTokenMinted { id, amount, to });
         }
         // 3. if the token id does not exist, return an error
         Err(MTKError::TokenDoesNotExists)
     }
 
-    fn add_ft(&mut self, ft_id: TokenId, nft_id: TokenId) -> Result<MTKEvent, MTKError> {
-        // if token id doens't exist, add it to the list of fungible tokens; else return an error
-        if !self.tokens.fungible_tokens.contains(&ft_id) {
-            self.tokens.fungible_tokens.push(ft_id);
-            self.tokens.balances.insert(ft_id, HashMap::new());
-            // add the matching pairs
-            self.tokens.matching_pairs.insert(ft_id, nft_id);
+    fn mint_nft_to(
+        &mut self,
+        to: ActorId,
+        metadata: SkillNftMetadata,
+    ) -> Result<MTKEvent, MTKError> {
+        // check if the name in the metadata exists in the available skill names
+        if !self
+            .tokens
+            .available_skill_names
+            .check_name(&metadata.title.clone().unwrap())
+        {
+            return Err(MTKError::SkillNameDoesNotExists);
+        }
+        // 1. generate a token id for the soon to be minted NFT.
+        let id = gen_token_id();
+        // 2. check if the token id already exists
+        if self.tokens.skill_nft_metadata.contains_key(&id) {
+            Err(MTKError::TokenAlreadyExists)
+        } else {
+            // 3. if the token id does not exist, add it to the list of NFTs
+            self.tokens.skill_nft_metadata.insert(id, metadata);
+            // 4. add the NFT to the list of NFTs owned by the receipient
+            self.tokens
+                .nft_owners
+                .entry(to)
+                .or_insert_with(Vec::new)
+                .push(id);
+            Ok(MTKEvent::SkillNftMinted { id, to })
+        }
+    }
 
-            return Ok(MTKEvent::NewFtAdded { ft_id, nft_id });
+    fn burn(&mut self, id: TokenId, from: ActorId, amount: u128) -> Result<MTKEvent, MTKError> {
+        // 1. check if the token id exists
+        if self.tokens.skill_fungible_tokens.contains_key(&id) {
+            // 2. if the owner exists, subtract from the existing balance; else return an error
+            let owner_balances = self.tokens.balances.get_mut(&id).unwrap();
+            if let Some((_owner, balance)) =
+                owner_balances.iter_mut().find(|(owner, _)| from.eq(owner))
+            {
+                if *balance < amount {
+                    return Err(MTKError::InsufficientBalance);
+                }
+                *balance -= amount;
+                return Ok(MTKEvent::SkillTokenBurned { id, amount, from });
+            }
+            return Err(MTKError::OwnerDoesNotExists);
+        }
+        // 3. if the token id does not exist, return an error
+        Err(MTKError::TokenDoesNotExists)
+    }
+
+    fn add_ft(&mut self, token_data: SkillFtData) -> Result<MTKEvent, MTKError> {
+        // only contract creator can add a new fungible token
+        if msg::source() != self.creator {
+            return Err(MTKError::OnlyCreaterCanOperate);
         }
 
-        Err(MTKError::TokenAlreadyExists)
+        let id = gen_token_id();
+        self.tokens
+            .skill_fungible_tokens
+            .insert(id.clone(), token_data);
+        self.tokens.balances.insert(id.clone(), HashMap::new());
+        Ok(MTKEvent::NewFtAdded { id })
+    }
+
+    fn change_ft(&mut self, id: TokenId, new_data: SkillFtData) -> Result<MTKEvent, MTKError> {
+        // only contract creator can add a new fungible token
+        if msg::source() != self.creator {
+            return Err(MTKError::OnlyCreaterCanOperate);
+        }
+
+        // check if the token id exists
+        if self.tokens.skill_fungible_tokens.contains_key(&id) {
+            self.tokens.skill_fungible_tokens.insert(id, new_data);
+            Ok(MTKEvent::SkillFtChanged { id })
+        } else {
+            Err(MTKError::TokenDoesNotExists)
+        }
+    }
+
+    fn change_base_uri(&mut self, new_uri: String) -> Result<MTKEvent, MTKError> {
+        self.tokens.base_uri = new_uri.clone();
+        Ok(MTKEvent::MtkUriChanged { new_uri })
+    }
+
+    fn add_nft(&mut self, name: String) -> Result<MTKEvent, MTKError> {
+        // only contract creator can add a new skill nft.
+        if msg::source() != self.creator {
+            return Err(MTKError::OnlyCreaterCanOperate);
+        }
+
+        // add the new name to the available names set.
+        self.tokens.available_skill_names.add_name(name.clone());
+        Ok(MTKEvent::SkillNftAdded { name })
+    }
+
+    fn remove_nft(&mut self, name: String) -> Result<MTKEvent, MTKError> {
+        // only contract creator can add a new skill nft.
+        if msg::source() != self.creator {
+            return Err(MTKError::OnlyCreaterCanOperate);
+        }
+
+        // remove the name from the available names set.
+        if self.tokens.available_skill_names.remove_name(&name) {
+            Ok(MTKEvent::SkillNftRemoved { name })
+        } else {
+            Err(MTKError::SkillNameDoesNotExists)
+        }
+    }
+
+    fn verify_repu(
+        &mut self,
+        target: ActorId,
+        skill_type: bool,
+        token_id: TokenId,
+    ) -> Result<MTKEvent, MTKError> {
+        if skill_type {
+            // search for the token id in the owner's NFTs
+            if self.tokens.nft_owners.contains_key(&target) {
+                if self
+                    .tokens
+                    .nft_owners
+                    .get(&target)
+                    .unwrap()
+                    .contains(&token_id)
+                {
+                    Ok(MTKEvent::RepuVerified {
+                        initiator: msg::source(),
+                        target,
+                    })
+                } else {
+                    Ok(MTKEvent::RepuVerificationFail {
+                        initiator: msg::source(),
+                        target,
+                    })
+                }
+            } else {
+                Ok(MTKEvent::RepuVerificationFail {
+                    initiator: msg::source(),
+                    target,
+                })
+            }
+        } else {
+            // search for the token id in the owner's FTs
+            if self.tokens.ft_owners.contains_key(&target) {
+                if self
+                    .tokens
+                    .ft_owners
+                    .get(&target)
+                    .unwrap()
+                    .contains(&token_id)
+                {
+                    Ok(MTKEvent::RepuVerified {
+                        initiator: msg::source(),
+                        target,
+                    })
+                } else {
+                    Ok(MTKEvent::RepuVerificationFail {
+                        initiator: msg::source(),
+                        target,
+                    })
+                }
+            } else {
+                Ok(MTKEvent::RepuVerificationFail {
+                    initiator: msg::source(),
+                    target,
+                })
+            }
+        }
     }
 }
 
@@ -105,46 +261,50 @@ impl From<Mtk> for State {
         let Mtk {
             tokens,
             creator,
-            supply,
         } = value;
 
         let MtkData {
             name,
             symbol,
             base_uri,
+            available_skill_names,
             balances,
-            matching_pairs,
-            fungible_tokens,
-            skill_nfts,
-            repu_nfts,
-            token_metadata,
-            owners,
+            skill_fungible_tokens,
+            ft_owners,
+            skill_nft_metadata,
+            nft_owners,
         } = tokens;
 
         let balances = balances
             .into_iter()
             .map(|(k, v)| (k, v.into_iter().map(|(a, b)| (a, b)).collect()))
             .collect();
-        let matching_pairs = matching_pairs.into_iter().collect();
-        let ft_tokens = fungible_tokens;
-        let skill_nft_tokens = skill_nfts;
-        let repu_nft_tokens = repu_nfts;
-        let token_metadata = token_metadata.into_iter().map(|(k, v)| (k, v)).collect();
-        let owners = owners.into_iter().map(|(k, v)| (k, v)).collect();
-        let supply = supply.into_iter().map(|(k, v)| (k, v)).collect();
+        let skill_fungible_tokens = skill_fungible_tokens
+            .into_iter()
+            .map(|(k, v)| (k, v))
+            .collect();
+        let ft_owners = ft_owners.into_iter().map(|(k, v)| (k, v)).collect();
+        let skill_nft_metadata = skill_nft_metadata
+            .into_iter()
+            .map(|(k, v)| (k, v))
+            .collect();
+        let nft_owners = nft_owners.into_iter().map(|(k, v)| (k, v)).collect();
+
         Self {
             name,
             symbol,
             base_uri,
-            balances,
-            matching_pairs,
-            ft_tokens,
-            skill_nft_tokens,
-            repu_nft_tokens,
-            token_metadata,
-            owners,
             creator,
-            supply,
+            available_skill_names,
+            balances,
+            skill_fungible_tokens,
+            ft_owners,
+            skill_nft_metadata,
+            nft_owners,
         }
     }
+}
+
+fn gen_token_id() -> TokenId {
+    exec::block_timestamp().into()
 }
